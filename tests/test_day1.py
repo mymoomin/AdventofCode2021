86066fceb1776c9e8559bed4e3c73fc562a642ec
mymoomin <20903941+mymoomin@users.noreply.github.com>
2021-12-02 20:48:21 +0000

    Add hypothesis tests

--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,4 +1,6 @@
 from AoC2021.solutions.day1 import main, part1, part2
+from hypothesis import given
+from hypothesis.strategies import lists, integers
 
 
 def get_input(filename: str):
@@ -11,6 +13,20 @@ demo_data = get_input("day1")
 
 def test_part1():
     assert part1(demo_data) == 7
+
+
+@given(lists(integers().map(str), min_size=1))
+def fuzz_part1(data):
+    count = part1(data)
+    assert count >= 0
+    assert count <= len(data) - 1
+
+
+@given(lists(integers().map(str), min_size=3))
+def fuzz_part2(data):
+    count = part2(data)
+    assert count >= 0
+    assert count <= len(data) - 3
 
 
 def test_part2():
