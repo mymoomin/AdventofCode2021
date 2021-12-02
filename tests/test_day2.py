1944b9ff87d337ca667b1a920c260b2b9a94eb18
mymoomin <20903941+mymoomin@users.noreply.github.com>
2021-12-02 21:18:45 +0000

    Check day 2 part 1 passes demo data

---
+++ tests/test_day2.py
@@ -0,0 +1,15 @@
+from AoC2021.solutions.day2 import main, part1, part2
+from hypothesis import given
+from hypothesis.strategies import lists, integers
+
+
+def get_input(filename: str):
+    with open(f"tests/data/{filename}.txt", "r") as f:
+        return f.read().split("\n")
+
+
+demo_data = get_input("day2demo")
+
+
+def test_part1():
+    assert part1(demo_data) == 150
