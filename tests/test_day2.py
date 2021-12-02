a0c2e9174bfdcd4d4d872e0b2c38d0d4e725b847
mymoomin <20903941+mymoomin@users.noreply.github.com>
2021-12-02 21:30:53 +0000

    Check all paths of day 2 parts 1 and 2

--- tests/test_day2.py
+++ tests/test_day2.py
@@ -14,10 +14,14 @@ demo_data = get_input("day2demo")
 
 def test_part1():
     assert part1(demo_data) == 150
+    with pytest.raises(ValueError):
+        part1(["back 3"])
 
 
 def test_part2():
     assert part2(demo_data) == 900
+    with pytest.raises(ValueError):
+        part2(["back 3"])
 
 
 def test_main():
